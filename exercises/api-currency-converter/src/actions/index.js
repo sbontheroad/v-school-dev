ff75e44243446697470b6415e1bbdd29f9b31d12
Sarah Baldwin <sarahb1245@gmail.com>
2017-07-25 17:54:54 -0600

    updated currency background

--- exercises/api-currency-converter/src/actions/index.js
+++ exercises/api-currency-converter/src/actions/index.js
@@ -15,6 +15,7 @@ export function getInfo(currency, amount) {
 function convert(data, currency, amount) {
   console.log(data);
   currency = currency.toUpperCase();
+  //take array and put key in brackets and it will return value of key
   let newAmount = (data[currency] * amount).toFixed(2);
   //find match of currency in response.data.rates
   //take value and multiply it by the input amount
